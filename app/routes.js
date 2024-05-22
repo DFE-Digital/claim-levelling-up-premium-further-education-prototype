861d8ccf9216b69af674a39c99a72f1d22289923
siddiqah-ii <siddiqahiislam@gmail.com>
2024-05-22 14:07:48 +0100

    branching updates

--- app/routes.js
+++ app/routes.js
@@ -23,4 +23,62 @@ router.post('/fe-provider', function (req, res){
       res.redirect('/academic-year-in-further-education');
     }
   });
+
   
+  router.post('/academic-year-in-further-education', function (req, res){
+    var oneFullTerm = req.session.data['term'];
+    if (oneFullTerm === 'No, I have not worked one full term before applying') {
+      res.redirect('/you-are-not-eligible-yet');
+    } else {
+      res.redirect('/academic-year-in-further-education');
+    }
+  });
+
+  router.post('/teaching-hours-per-week', function (req, res){
+    var academicYearInFurtherEducation = req.session.data['year'];
+    if (academicYearInFurtherEducation == 'Before 2020 to 2021') {
+      res.redirect('/you-are-not-eligible-five-years');
+    } else {
+      res.redirect('/teaching-hours-per-week');
+    }
+  });
+  
+  router.post('/half-teaching-hours', function (req, res) {
+    var teachingHoursPerWeek = req.session.data['week'];
+    if (teachingHoursPerWeek == 'Less than 2.5 hours per week') {
+      res.redirect('/not-eligible-hours-spent-teaching');
+    } else {
+      res.redirect('/half-teaching-hours');
+    }
+  });
+
+  router.post('/half-teaching-hours', function (req, res) {
+    var teachingHoursPerWeekVariable = req.session.data['week'];
+    if (teachingHoursPerWeekVariable == 'Less than 2.5 hours per week') {
+      res.redirect('/not-eligible-hours-spent-teaching');
+    } else {
+      res.redirect('/half-teaching-hours');
+    }
+  });
+
+  router.post('/subject-areas', function (req, res) {
+    var halfTeachingHours = req.session.data['hoursHalf'];
+    if (halfTeachingHours == 'No') {
+      res.redirect('/not-eligible-16-19');
+    } else {
+      res.redirect('/subject-areas');
+    }
+  });
+
+
+
+
+
+  router.post('/qualification', function (req, res) {
+    var teachingCourses = req.session.data['courses-2'];
+    if (teachingCourses == 'No') {
+      res.redirect('/not-eligible-teach-half-hours-courses');
+    } else {
+      res.redirect('/qualification');
+    }
+  });
