5cce6e65d976b0b75ce31aee7a4fca57239ec16d
Tom <tommy_brown@me.com>
2025-04-24 11:59:36 +0100

    Added GET route and logic to handle form on show.html --> check.html

--- app/routes/claims.js
+++ app/routes/claims.js
@@ -11,4 +11,28 @@ module.exports = router => {
     res.render('provider/show', { claim })
   })
 
+
+  router.post('/provider/check/:claimId', (req, res) => {
+    res.redirect(`/provider/check/${req.params.claimId}`)
+  })
+  
+
+
+  router.get('/provider/check/:claimId', (req, res) => {
+    const claims = req.session.data.claims || []
+    const claim = claims.find(c => String(c.id) === req.params.claimId)
+  
+    if (!claim) {
+      return res.status(404).send('Claim not found')
+    }
+  
+    res.render('provider/check', { claim })
+  })
+  
+
+ 
+  
+  
+
+
 }
