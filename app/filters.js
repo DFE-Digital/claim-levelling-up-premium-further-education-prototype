54ac0b9589a9e0991debd00fa29716a3f4a5a05e
Tom <tommy_brown@me.com>
2025-04-25 17:07:57 +0100

    Added switch statemant for tag statusColourto filters.js + index.html + show.html

--- app/filters.js
+++ app/filters.js
@@ -7,4 +7,14 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const addFilter = govukPrototypeKit.views.addFilter
 
 // Add your filters here
+addFilter('statusColour', status => {
+  switch(status) {
+    case 'In progress':
+      return 'govuk-tag--purple'
+    case 'Not started':
+      return 'govuk-tag--red'
+    case 'Complete':
+      return 'govuk-tag--green'
+  }
+})
 
