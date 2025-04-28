a534792dd7b9f293a37a45850d8dc7f53db26e30
Tom <tommy_brown@me.com>
2025-04-28 17:12:50 +0100

    Revert "commit for revert"
    This reverts commit b205c2c3caf3b6e666b2fb5061d1bbb4b34435ed.

--- app/routes/claims.js
+++ app/routes/claims.js
@@ -19,7 +19,7 @@ module.exports = router => {
 
 
 
-  router.get('/provider/show/:claimId', (req, res) => {
+  router.get('/provider/:claimId', (req, res) => {
     let claims = req.session.data.claims || []
     let claim = claims.find(claim => String(claim.id) === req.params.claimId)
   
@@ -30,17 +30,9 @@ module.exports = router => {
     res.render('provider/show', { claim })
   })
 
-   // Handle completing the claim
+
   router.post('/provider/check/:claimId', (req, res) => {
-    const claims = req.session.data.claims || []
-    const claim = claims.find(c => String(c.id) === req.params.claimId)
-
-    if (!claim) {
-      return res.status(404).send('Claim not found')
-    }
-
     const selectedIssues = req.body.incorrect
-
 
     // Save checkboxes on show.html to session
     req.session.data.incorrect = Array.isArray(selectedIssues)
@@ -49,23 +41,26 @@ module.exports = router => {
     ? [selectedIssues]           // Single string — wrap it in an array
     : []                         // Nothing selected — fallback to an empty array
 
-    claim.status = 'Complete'
-    claim.assignedTo = 'Tom Brown'
-    claim.assignedDate = new Date().toISOString()
-
     res.redirect(`/provider/check/${req.params.claimId}`)
   })
   
 
-  router.get('/provider/check/:claimId', (req, res) => {
-    let claims = req.session.data.claims || []
-    let claim = claims.find(claim => String(claim.id) === req.params.claimId)
 
+  router.get('/provider/check/:claimId', (req, res) => {
+    const claims = req.session.data.claims || []
+    const claim = claims.find(c => String(c.id) === req.params.claimId)
+  
     if (!claim) {
       return res.status(404).send('Claim not found')
     }
-
+  
     res.render('provider/check', { claim })
   })
+  
+
+ 
+  
+  
+
 
 }
