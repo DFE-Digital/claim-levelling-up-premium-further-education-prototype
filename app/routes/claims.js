cadaf7b4c714e98fdc73f34204d4fbae38335e16
Tom <tommy_brown@me.com>
2025-04-29 09:39:19 +0100

    Added skeleton for /provider/verified/claimId + conditiona link in /provider/index + route in routes.js

--- app/routes/claims.js
+++ app/routes/claims.js
@@ -17,9 +17,8 @@ module.exports = router => {
     })
   })
 
-
-
-  router.get('/provider/:claimId', (req, res) => {
+  //Handles the link to show.html
+  router.get('/provider/show/:claimId', (req, res) => {
     let claims = req.session.data.claims || []
     let claim = claims.find(claim => String(claim.id) === req.params.claimId)
   
@@ -30,7 +29,19 @@ module.exports = router => {
     res.render('provider/show', { claim })
   })
 
+   //Handles the link to verified.html
+   router.get('/provider/verified/:claimId', (req, res) => {
+    let claims = req.session.data.claims || []
+    let claim = claims.find(claim => String(claim.id) === req.params.claimId)
+  
+    if (!claim) {
+      return res.status(404).send('Claim not found')
+    }
+  
+    res.render('provider/verified', { claim })
+  })
 
+  //Handles the checkboxes on show.html
   router.post('/provider/check/:claimId', (req, res) => {
     const selectedIssues = req.body.incorrect
 
@@ -43,8 +54,6 @@ module.exports = router => {
 
     res.redirect(`/provider/check/${req.params.claimId}`)
   })
-  
-
 
   router.get('/provider/check/:claimId', (req, res) => {
     const claims = req.session.data.claims || []
