5f204877d45f3987c1beeb06c78d608890c3fa59
Tom <tommy_brown@me.com>
2025-04-30 11:41:32 +0100

    Updated diciplinary questions on verify-details-radios.njk -> they now need a 'no' anserw to verify

--- app/routes/claims--complete.js
+++ app/routes/claims--complete.js
@@ -2,21 +2,6 @@ const Pagination = require('../helpers/pagination')
 
 module.exports = router => {
 
-  // router.get('/provider/completed', (req, res) => {
-  //   const claims = req.session.data.claims || []
-
-  //   const completedClaims = claims.filter(claim => claim.status === 'Complete')
-    
-  //   let pageSize = 10
-  //   let pagination = new Pagination(completedClaims, req.query.page, pageSize)
-  //   const paginatedClaims = pagination.getData()
-    
-  //   res.render('provider/completed/index', {
-  //     claims: paginatedClaims,
-  //     pagination
-  //   })
-  // })
-
   router.get('/provider/completed/show/:claimId', (req, res) => {
     const claims = req.session.data.claims || []
     const claim = claims.find(c => String(c.id) === req.params.claimId)
