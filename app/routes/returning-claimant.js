d1771c640c6523c01ea5a126cea7af41728fb440
Tom <tommy_brown@me.com>
2025-06-10 15:43:35 +0100

    Added all basic validation for empty fields to /one-login-returning-claimant journey

--- app/routes/returning-claimant.js
+++ app/routes/returning-claimant.js
@@ -1,25 +1,40 @@
 module.exports = router => {
 
-  router.post('/one-login-returning-claimant/received-retention-incentive-before', (req, res) => {
-    let data = req.session.data
-    let receivedRetentionIncentive = data.receivedRetentionIncentive
-    if (receivedRetentionIncentive === 'Yes') {
-      ///this was previously /one-login-returning-claimant/one-login-sign-in-only
-      res.redirect('/one-login-returning-claimant/do-you-have-a-one-login-account')
-    } else{
-      res.redirect('/one-login-returning-claimant/do-you-have-a-one-login-account')
-    }
-  })
+router.post('/one-login-returning-claimant/received-retention-incentive-before', (req, res) => {
+  const received = req.session.data.receivedRetentionIncentive
+  if (!received) {
+    return res.render('one-login-returning-claimant/received-retention-incentive-before', {
+      error: {
+        text: 'Select an option',
+        href: '#receivedRetentionIncentive'
+      },
+      data: req.session.data
+    })
+  }
+  res.redirect('/one-login-returning-claimant/do-you-have-a-one-login-account');
+})
 
-  router.post('/one-login-returning-claimant/do-you-have-a-one-login-account', (req, res) => {
-    let data = req.session.data
-    let hasOneLoginAccount = data.hasOneLoginAccount
-    if (hasOneLoginAccount === 'Yes' ||  hasOneLoginAccount === "I don't know") {
-      res.redirect('/one-login-returning-claimant/one-login-sign-in-only')
-    } else {
-      res.redirect('/start')
-    }
-  })
+
+router.post('/one-login-returning-claimant/do-you-have-a-one-login-account', (req, res) => {
+  const hasOneLoginAccount = req.session.data.hasOneLoginAccount
+
+  if (!hasOneLoginAccount) {
+    return res.render('one-login-returning-claimant/do-you-have-a-one-login-account', {
+      error: {
+        text: 'Select if you have a GOV.UK One Login account',
+        href: '#hasOneLoginAccount'
+      },
+      data: req.session.data
+    })
+  }
+
+  const nextStep = (hasOneLoginAccount === 'Yes' || hasOneLoginAccount === "I don't know")
+    ? '/one-login-returning-claimant/one-login-sign-in-only'
+    : '/start'
+
+  res.redirect(nextStep)
+})
+
 
   router.post('/one-login-returning-claimant/one-login-sign-in-only', (req, res) => {
     let data = req.session.data
@@ -28,25 +43,58 @@ module.exports = router => {
     
   })
 
+
+  /////////// ONE LOGIN SIGN IN ////////////
   router.post('/one-login-returning-claimant/one-login-email', (req, res) => {
-    let data = req.session.data
-    
+    const oneLoginEmail = req.session.data.oneLoginEmail
+
+    if (!oneLoginEmail) {
+      return res.render('one-login-returning-claimant/one-login-email', {
+        error: {
+          text: 'Enter your One Login email address',
+          href: '#one-login-email'
+        },
+        data: req.session.data
+      })
+    }
+
     res.redirect('/one-login-returning-claimant/one-login-password')
-    
   })
 
+  /////////// ONE LOGIN PASSWORD ////////////
   router.post('/one-login-returning-claimant/one-login-password', (req, res) => {
-    let data = req.session.data
-    
-    res.redirect('/one-login-returning-claimant/one-login-one-time-passcode')
-    
-  })
+  const oneLoginPassword = req.session.data.oneLoginPassword
+  if (!oneLoginPassword) {
+    return res.render('one-login-returning-claimant/one-login-password', {
+      error: {
+        text: 'Enter your One Login password',
+        href: '#one-login-password'
+      },
+      data: req.session.data
+    })
+  }
+  res.redirect('/one-login-returning-claimant/one-login-one-time-passcode');
+})
+
+
+
+  /////////// ONE LOGIN ONE TIME PASSCODE ////////////
   router.post('/one-login-returning-claimant/one-login-one-time-passcode', (req, res) => {
-    let data = req.session.data
-    
-    res.redirect('/one-login-returning-claimant/one-login-signed-in')
-    
+    const securityCode = req.session.data.securityCode
+    if (!securityCode) {
+      return res.render('one-login-returning-claimant/one-login-one-time-passcode', {
+        error: {
+          text: 'Enter the 6-digit security code',
+          href: '#security-code'
+        },
+        data: req.session.data
+      })
+    }
+    res.redirect('/one-login-returning-claimant/one-login-signed-in');
   })
+
+
+
 
   router.post('/one-login-returning-claimant/one-login-signed-in', (req, res) => {
     let data = req.session.data
@@ -55,35 +103,84 @@ module.exports = router => {
     
   })
 
+  /////////// FIND ANOTHER WAY ////////////
   router.post('/one-login-returning-claimant/find-another-way', (req, res) => {
-    let data = req.session.data
-    if (data.findAnotherWay === "continue") {
-       res.redirect('/one-login-returning-claimant/confirm-college')   
-    } else {
-      res.redirect('https://govuk-one-login-prototype-6d2545e2d700.herokuapp.com/page-index/authentication/create-account')  
-    } 
-  })
-
-  router.post('/one-login-returning-claimant/confirm-college', (req, res) => {
-    let data = req.session.data
-    if (data.hasWorkEmail === "Yes") {
-      res.redirect('/one-login-returning-claimant/work-email')
-      
-    } else {
-      
+    const choice = req.session.data.findAnotherWay
+    if (!choice) {
+      return res.render('one-login-returning-claimant/find-another-way', {
+        error: {
+          text: 'Select an option',
+          href: '#findAnotherWay'
+        },
+        data: req.session.data
+      })
     }
-    res.redirect('/one-login-returning-claimant/signed-you-out')
+    if (choice === 'continue') {
+      res.redirect('/one-login-returning-claimant/confirm-college');
+    } else {
+      res.redirect('https://govuk-one-login-prototype-6d2545e2d700.herokuapp.com/page-index/authentication/create-account');
+    }
+  })
+
+
+
+  /////////// CONFIRM COLLEGE WORK EMAIL ACCOUNT ////////////
+  router.post('/one-login-returning-claimant/confirm-college', (req, res) => {
+    const hasWorkEmail = req.session.data.hasWorkEmail
     
+    if (!hasWorkEmail) {
+      return res.render('one-login-returning-claimant/confirm-college', {
+        error: {
+          text: 'Select if you have access to your college email account',
+          href: '#hasWorkEmail'
+        },
+        data: req.session.data
+      })
+    }
+    const nextStep = (hasWorkEmail === 'Yes')
+      ? '/one-login-returning-claimant/work-email'
+      : '/one-login-returning-claimant/signed-you-out'
+    res.redirect(nextStep)
   })
 
+  
+  /////////// ENTER WORK EMAIL  ////////////
   router.post('/one-login-returning-claimant/work-email', (req, res) => {
-    let data = req.session.data
-    res.redirect('/one-login-returning-claimant/six-digit-passcode') 
+    let workEmail = req.session.data.workEmail
+    if (!workEmail) {
+      return res.render('one-login-returning-claimant/work-email', {
+        error: {
+          text: 'Enter your work email address',
+          href: '#workEmail'
+        },
+        data: req.session.data
+      })
+    }
+    const nextStep = (workEmail !== '' && workEmail !== undefined)
+        ? '/one-login-returning-claimant/six-digit-passcode'
+        : '/work-email'
+
+      res.redirect(nextStep)
   })
 
+
+
+
+  /////////// SIX DIGIT PASSCODE ////////////
   router.post('/one-login-returning-claimant/six-digit-passcode', (req, res) => {
-    let data = req.session.data
-    res.redirect('/one-login-returning-claimant/how-we-will-use-your-information') 
+    const sixDigitSecurityCode = req.session.data.sixDigitSecurityCode
+
+    if (!sixDigitSecurityCode) {
+      return res.render('one-login-returning-claimant/six-digit-passcode', {
+        error: {
+          text: 'Enter the 6 digit security code',
+          href: '#six-digit-security-code'
+        },
+        data: req.session.data
+      })
+    }
+
+    res.redirect('/one-login-returning-claimant/how-we-will-use-your-information');
   })
 
   router.post('/one-login-returning-claimant/how-we-will-use-your-information', (req, res) => {
@@ -91,8 +188,84 @@ module.exports = router => {
     res.redirect('/one-login-returning-claimant/personal-details') 
   })
 
-    router.post('/one-login-returning-claimant/personal-details', (req, res) => {
-    let data = req.session.data
-    res.redirect('/one-login-returning-claimant/personal-bank-details') 
+  /////////// PERSONAL DETAILS //////////// 
+  router.post('/one-login-returning-claimant/personal-details', (req, res) => {
+    const data = req.session.data
+    const errors = []
+    const fieldErrors = {}
+
+    const day = data['returnClaimantDob-day']
+    const month = data['returnClaimantDob-month']
+    const year = data['returnClaimantDob-year']
+
+    if (!data.returnClaimantFirstName) {
+      const message = 'Enter your first name'
+      errors.push({ text: message, href: '#return-claimant-first-name' })
+      fieldErrors.returnClaimantFirstName = { text: message }
+    }
+
+    if (!data.returnClaimantLastName) {
+      const message = 'Enter your last name'
+      errors.push({ text: message, href: '#return-claimant-last-name' })
+      fieldErrors.returnClaimantLastName = { text: message }
+    }
+
+    if (!day || !month || !year) {
+      const message = 'Enter your full date of birth'
+      errors.push({ text: message, href: '#return-claimant-dob' })
+      fieldErrors.returnClaimantDob = { text: message }
+    }
+
+    if (!data.returnClaimantNationalInsuranceNumber) {
+      const message = 'Enter your National Insurance number'
+      errors.push({ text: message, href: '#return-claimant-national-insurance-number' })
+      fieldErrors.returnClaimantNationalInsuranceNumber = { text: message }
+    }
+
+    if (errors.length) {
+      return res.render('one-login-returning-claimant/personal-details', {
+        errors,
+        fieldErrors,
+        data
+      })
+    }
+
+    res.redirect('/one-login-returning-claimant/personal-bank-details')
   })
+
+  /////////// PERSONAL BANK DETAILS ////////////
+  router.post('/one-login-returning-claimant/personal-bank-details', (req, res) => {
+  const data = req.session.data
+  const errors = []
+  const fieldErrors = {}
+
+  if (!data.returnClaimantBankAccountName) {
+    const message = 'Enter the name on the account'
+    errors.push({ text: message, href: '#return-claimant-bank-account-name' })
+    fieldErrors.returnClaimantBankAccountName = { text: message }
+  }
+
+  if (!data.returnClaimantSortCode) {
+    const message = 'Enter a sort code'
+    errors.push({ text: message, href: '#return-claimant-sort-code' })
+    fieldErrors.returnClaimantSortCode = { text: message }
+  }
+
+  if (!data.returnClaimantAccountNumber) {
+    const message = 'Enter an account number'
+    errors.push({ text: message, href: '#return-claimant-account-number' })
+    fieldErrors.returnClaimantAccountNumber = { text: message }
+  }
+
+  if (errors.length) {
+    return res.render('one-login-returning-claimant/personal-bank-details', {
+      errors,
+      fieldErrors,
+      data
+    })
+  }
+
+    res.redirect('/one-login-returning-claimant/success'); // or whatever next step
+  })
+
 }
