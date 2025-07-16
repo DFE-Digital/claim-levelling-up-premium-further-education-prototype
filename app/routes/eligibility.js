7eeeaaf868d462e3ac85f666dad678229a57f75b
Tom <tommy_brown@me.com>
2025-07-16 11:59:23 +0100

    Created fe-provider template and installed autocomplete component

--- app/routes/eligibility.js
+++ app/routes/eligibility.js
@@ -10,6 +10,27 @@ module.exports = router => {
       res.redirect('/eligibility/teaching-qualification')
   })
 
+  ////////////////////// TEACHING QUALIFICATION. //////////////////////
+  router.post('/eligibility/teaching-qualification', (req, res) => {
+    const claimantTeachingQualification = req.body.claimantTeachingQualification
+
+    if (claimantTeachingQualification === 'No, and I do not plan to enrol on one in the next 12 months') {
+      res.redirect('/eligibility/not-eligible/not-eligible')
+    }
+      res.redirect('/eligibility/teaching-responsibilities')
+  })
+
+  ////////////////////// TEACHING QUALIFICATION. //////////////////////
+  router.post('/eligibility/teaching-responsibilities', (req, res) => {
+    const claimantTeachingResponsibilities = req.body.claimantTeachingResponsibilities
+
+    if (claimantTeachingResponsibilities === 'No') {
+      res.redirect('/eligibility/not-eligible/not-eligible')
+    }
+     res.redirect('/eligibility/fe-provider')
+  })
+
+
 
 }
 
